cf16f399ba0f6c60c19a0284e4e20fcdfb245ef8
Jordy <j.naus@student.ru.nl>
2021-01-12 08:30:24 +0100

    topic distribution per document

--- src/LDA.py
+++ src/LDA.py
@@ -37,6 +37,38 @@ def LDA(tweets, k = 1):
     tweets_corpus = [tweets_dictionary.doc2bow(tweet) for tweet in tweets.tokenized_tweet]
     return LdaModel(tweets_corpus, num_topics = k, id2word = tweets_dictionary, passes=10)
 
+def assign_topics_to_tweets(tweets, tweets_lda):
+    tweets_dictionary = Dictionary(tweets.tokenized_tweet)
+    tweets_corpus = [tweets_dictionary.doc2bow(tweet) for tweet in tweets.tokenized_tweet]
+    topic_0 = []
+    topic_1 = []
+    topic_2 = []
+    topic_3 = []
+    for text in tweets['tokenized_tweet']:
+        bow = tweets_dictionary.doc2bow(text)
+        topic_distribution = dict(tweets_lda.get_document_topics(bow))
+        if 0 in topic_distribution:
+            topic_0.append(topic_distribution[0])
+        else:
+            topic_0.append(0)
+        if 1 in topic_distribution:
+            topic_1.append(topic_distribution[1])
+        else:
+            topic_1.append(0)
+        if 2 in topic_distribution:
+            topic_2.append(topic_distribution[2])
+        else:
+            topic_2.append(0)
+        if 3 in topic_distribution:
+            topic_3.append(topic_distribution[3])
+        else:
+            topic_3.append(0)
+    tweets['topic_0'] = topic_0
+    tweets['topic_1'] = topic_1
+    tweets['topic_2'] = topic_2
+    tweets['topic_3'] = topic_3
+    return tweets#tweets_lda.get_document_topics(bow)
+    
 def LDA_OT(lda, tweets, time_slices, k = 1):
     tweets_dictionary = Dictionary(tweets.tokenized_tweet)
     tweets_corpus = [tweets_dictionary.doc2bow(tweet) for tweet in tweets.tokenized_tweet]
--- src/main.py
+++ src/main.py
@@ -3,9 +3,10 @@ import json
 import shutil
 import pandas as pd
 from DataLoader import load_data, clean_data, preprocess
-from LDA import getWordFreq, computeCoherence, LDA, LDA_OT
+from LDA import getWordFreq, computeCoherence, LDA, LDA_OT, assign_topics_to_tweets
 from utils import plotCoherence, plot_top_words
 from gensim.models import ldaseqmodel
+from gensim.models.ldamodel import LdaModel
 
 def main():
     if not os.path.isfile("preprocessed_tweets.csv"):
@@ -18,32 +19,21 @@ def main():
         tweets.to_csv('preprocessed_tweets.csv', index = False)
     else:
         tweets = pd.read_csv('preprocessed_tweets.csv', converters={"tokenized_tweet": lambda x: x.strip("[]").split(", ")})
-    
-    if not os.path.isfile("lda_over_time"):
-        month_counts = tweets['month'].value_counts().to_dict()
-        time_slices = []
-        time_slices.append(month_counts['Feb'])
-        time_slices.append(month_counts['Mar'])
-        time_slices.append(month_counts['Apr'])
-        time_slices.append(month_counts['May'])
 
+    if not os.path.exists("tweets_lda"):
         tweets_lda = LDA(tweets, k=4)
-        print("LDA done")
-        lda_ot = LDA_OT(tweets_lda, tweets, time_slices, k = 4)
-        print("Finished TOT")
-        lda_ot.save('lda_over_time')
+        tweets_lda.save('tweets_lda')
     else:
-        lda_ot = ldaseqmodel.LdaSeqModel.load('lda_over_time')
-    
-    print(lda_ot.print_topic_times(0, top_terms=10))
-    #freqlist = getWordFreq(tweets)
-    #print(freqlist.most_common(10))
-    #tweet_coherence = computeCoherence(tweets)
-    #plotCoherence(tweet_coherence)
-    #for k in range(1,10):
-    #    print("performing LDA with {} topics".format(k))
-    #    tweets_lda = LDA(tweets, k)
-    #    plot_top_words(tweets_lda, k=k)
+        tweets_lda = LdaModel.load('tweets_lda')
+
+    #print(assign_topics_to_tweets(tweets, tweets_lda))
+
+    if not os.path.isfile("tweets_with_topics.csv"):
+        tweets_with_topics = assign_topics_to_tweets(tweets, tweets_lda)
+        tweets_with_topics.to_csv('tweets_with_topics.csv', index = False)
+    else:
+        tweets_with_topics = pd.read_csv('tweets_with_topics.csv', converters={"tokenized_tweet": lambda x: x.strip("[]").split(", ")})
+
 
 
 if __name__ == '__main__':
